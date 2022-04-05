de15e380cb10986c33acb4b0ebd4f61c2dd656d5
ThewBear <26767607+ThewBear@users.noreply.github.com>
2022-04-05 23:58:03 +0700

    Update main.js

--- main.js
+++ main.js
@@ -5,19 +5,24 @@ import airtable from 'airtable'
 program
     .addOption(new Option('-k, --key <key>', 'Airtable api key').env("AIRTABLE_API_KEY").makeOptionMandatory())
     .addOption(new Option('-b, --base <base>', 'Airtable base').env("AIRTABLE_BASE").makeOptionMandatory())
+    .addOption(new Option('-t, --table <base>', 'Airtable table').env("AIRTABLE_TABLE").makeOptionMandatory())
     .addOption(new Option('-u, --user <username>', 'SMTP username').env("USERNAME").makeOptionMandatory())
     .addOption(new Option('-p, --password <password>', 'SMTP password').env("PASSWORD").makeOptionMandatory())
-    .addOption(new Option('-f, --from <sender>', 'SMTP from ex. name <name@example.com>').env("FROM").makeOptionMandatory())
-    .option("-p, --page <number>", "Airtable pageSize, the number of records returned in each request.", 10)
+    .option("-p, --page <number>", "Airtable pageSize, the number of records returned in each request.", 100)
+    .option("--fieldFrom <string>", "Airtable from field", "from")
     .option("--fieldTo <string>", "Airtable to field", "to")
     .option("--fieldSubject <string>", "Airtable subject field", "subject")
     .option("--fieldHtml <string>", "Airtable html field", "html")
     .option("--fieldText <string>", "Airtable text field", "text")
-    .option("--maxConnections <number>", "nodemailer maxConnections", 10)
+    .option("--fieldAttachments <string>", "Airtable attachments field", "attachments")
+    .option("--fieldMessageId <string>", "Airtable messageId field", "messageId")
+    .option("--maxConnections <number>", "nodemailer maxConnections", 5)
+    .option("-d, --dry-run", "dry run")
+    .option("-v, --verbose", "debug")
     .parse();
 const options = program.opts();
 
-console.log(options)
+if (options.debug) console.log(options)
 
 airtable.configure({
     endpointUrl: 'https://api.airtable.com',
@@ -37,63 +42,52 @@ const transporter = nodemailer.createTransport({
 });
 await transporter.verify();
 console.timeEnd("createTransport");
-console.log(transporter)
 
-const base = airtable.base(options.base);
+const table = airtable.base(options.base)(options.table);
+const results = []
 
-base('Extern').select({
+await table.select({
     pageSize: options.page,
     view: "Grid view"
 }).eachPage(async function page(records, fetchNextPage) {
-    // This function (`page`) will get called for each page of records.
-
-    const messages = []
-
-    records.forEach(function (record) {
-        messages.push({
+    console.time(`sendMail ${records.length}`);
+    records.forEach(async function (record) {
+        let attachments
+        if (Array.isArray(record.get(options.fieldAttachments))) {
+            attachments = record.get(options.fieldAttachments).map(attachment=> ({
+                filename: attachment.filename,
+                href: attachment.url,
+                contentType: attachment.type
+            }))
+        }
+        const message = {
+            from: record.get(options.fieldFrom),
             to: record.get(options.fieldTo),
             subject: record.get(options.fieldSubject),
             text: record.get(options.fieldText),
             html: record.get(options.fieldHtml),
-        })
+            attachments
+        }
+
+        if (options.dryRun) {
+            console.log(message)
+            results.push(Promise.resolve(message))
+            return
+        }
+
+        // const result = transporter.sendMail(message)
+        //     .then((result) => record.updateFields({
+        //         [options.fieldMessageId]:  result.messageId
+        //     }))
+        //     .catch((err) => console.error(record.id, err))
+
+        // results.push(result)
     });
 
-    console.log(messages)
+    await Promise.all(results)
+    console.timeEnd(`sendMail ${records.length}`);
 
-    // console.time("sendMail");
-    // const statuses = messages.map((message) =>
-    //     transporter
-    //         .sendMail({
-    //             from: options.from,
-    //             to: message.to,
-    //             subject: message.subject,
-    //             text: message.text,
-    //             html: message.html,
-    //             attachments: message.attachments,
-    //         })
-    //         .then(
-    //             (success) => {
-    //                 console.log("Sent", message.id, message.to, success.messageId);
-    //                 return success;
-    //             },
-    //             (error) => {
-    //                 console.error("Error ", message.id, message.to, error);
-    //                 return error;
-    //             }
-    //         )
-    // );
-
-    // let infos = await Promise.all(statuses);
-    // console.timeEnd("sendMail");
-
-    // To fetch the next page of records, call `fetchNextPage`.
-    // If there are more records, `page` will get called again.
-    // If there are no more records, `done` will get called.
     fetchNextPage();
+}).catch(console.error)
 
-}, function done(err) {
-    if (err) { console.error(err); return; }
-});
-
-// console.log("Sending completed");
-// transporter.close();
+transporter.close();
